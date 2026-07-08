d01a53a8c1f48abec6e9b29fbec0c8bde4574868
qlrxn1152 <qlrxn1152@naver.com>
2026-07-08 16:19:53 +0900

    chore: initialize footmatch api project

---
+++ src/main/java/daehoon/football_v2/FootballV2Application.java
@@ -0,0 +1,13 @@
+package daehoon.football_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/daehoon/football_v2/FootballV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package daehoon.football_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
