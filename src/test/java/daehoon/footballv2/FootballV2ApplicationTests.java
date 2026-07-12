07acb00402ffcdf1b3f9d177bc6bcdf895c0a33c
qlrxn1152 <155734099+qlrxn1152@users.noreply.github.com>
2026-07-13 08:53:57 +0900

    test: load context with test profile

--- src/test/java/daehoon/footballv2/FootballV2ApplicationTests.java
+++ src/test/java/daehoon/footballv2/FootballV2ApplicationTests.java
@@ -2,8 +2,10 @@ package daehoon.footballv2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FootballV2ApplicationTests {
 
 	@Test
